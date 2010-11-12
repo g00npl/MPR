078902a582a93b0c00d1cc49fad07ac4bd5428ad
goon <goon@debian.(none)>
2010-11-13 00:13:03 +0100

    1210201006

--- src/pl/com/zoo/data/db/HsqlDataManger.java
+++ src/pl/com/zoo/data/db/HsqlDataManger.java
@@ -16,14 +16,16 @@ import java.util.Set;
 import pl.com.zoo.basic.Animal;
 import pl.com.zoo.basic.Class;
 import pl.com.zoo.data.DataManager;
-import pl.com.zoo.main.ZooKeeper;;
+import pl.com.zoo.main.ZooKeeper;
+
+;
 
 public class HsqlDataManger implements DataManager {
 	private Connection con;
 	private String animalTableName = "animal";
 	private String classTableName = "class";
 	private Statement stmt;
-	private String host = "localhost";
+	private String host = "10.0.0.7";
 	Animal animalgettall;
 	Class classgetall;
 	ZooKeeper zk;
@@ -71,25 +73,27 @@ public class HsqlDataManger implements DataManager {
 	public boolean addOrAnimalUpdate(Class cl, Animal animal) {
 		try {
 			PreparedStatement ps = con
-					.prepareStatement("Insert into animal(name, species, weight) values (?,?,?,?)");
+					.prepareStatement("Insert into animal(name, species, weight, id_class) values (?,?,?,?)");
 
 			ResultSet checkall = stmt
-					.executeQuery("Select 1 from animal where name='"
+					.executeQuery("Select id from animal where name='"
 							+ animal.getName() + "' and species='"
 							+ animal.getSpecies() + "' and weight="
 							+ animal.getWeight());
 
 			ResultSet checkname = stmt
-					.executeQuery("Select 1 from animal where name='"
+					.executeQuery("Select id from animal where name='"
 							+ animal.getName() + "'");
+			checkname.next();
 
-			if (checkname.getInt(1) == 1) {
+			checkall.next();
+			if (checkall.getInt("id") > 0) {
+				fail("Animal already exist");
+			}
+
+			if (checkname.getInt("id") > 0) {
 				removeAnimal(animal);
 				addOrAnimalUpdate(cl, animal);
-			}
-			if (checkall.getInt(1) == 1) {
-				System.err.println("Animal already exist");
-				fail();
 			}
 
 			ps.setString(1, animal.getName());
@@ -105,33 +109,17 @@ public class HsqlDataManger implements DataManager {
 	}
 
 	@Override
-	public void removeAnimal(Animal animal) {
-		try {
-			PreparedStatement ps = con.prepareStatement("Delete from "
-					+ animalTableName + " where name='?'");
-			ps.setString(1, animal.getName());
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			System.err.println("You want remove animal which not exist");
-			fail();
-			// e.printStackTrace();
-		}
-
-	}
-
-	@Override
 	public int addOrUpdateClass(Class cl) {
 		try {
 			PreparedStatement ps = con.prepareStatement("Insert into "
 					+ classTableName + "(name) values (?)");
 
 			ResultSet checkname = stmt
-					.executeQuery("Select 1 from class where name='"
+					.executeQuery("Select id from class where name='"
 							+ cl.getName() + "'");
-
-			if (checkname.getInt(1) == 1) {
-			System.err.println("Class already exist!");
-			fail();
+			checkname.next();
+			if (checkname.getInt("id") > 0) {
+				fail("Class already exist");
 			}
 
 			ps.setString(1, cl.getName());
@@ -149,18 +137,34 @@ public class HsqlDataManger implements DataManager {
 	}
 
 	@Override
+	public void removeAnimal(Animal animal) {
+		try {
+			PreparedStatement ps = con.prepareStatement("Delete from "
+					+ animalTableName + " where name=?");
+			ps.setString(1, animal.getName());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			System.err.println("You want remove animal which not exist");
+			fail();
+			// e.printStackTrace();
+		}
+
+	}
+
+	@Override
 	public void removeClass(Class cl) {
 		try {
 			ResultSet rs = stmt.executeQuery("SELECT id from " + classTableName
 					+ " where name='" + cl.getName() + "'");
 
+			rs.next();
 			PreparedStatement ps1 = con.prepareStatement("Delete from "
 					+ animalTableName + " where id_class=?");
-			ps1.setInt(1, rs.getInt(1));
+			ps1.setInt(1, rs.getInt("id"));
 			ps1.executeUpdate();
 
 			PreparedStatement ps = con.prepareStatement("Delete from "
-					+ classTableName + " where name='?'");
+					+ classTableName + " where name=?");
 			ps.setString(1, cl.getName());
 			ps.executeUpdate();
 
@@ -180,22 +184,21 @@ public class HsqlDataManger implements DataManager {
 			while (rs.next()) {
 				String name = rs.getString("name");
 				int id = rs.getInt("id");
-
 				classgetall = new Class(name);
 
 				ResultSet rs1 = stmt
 						.executeQuery("SELECT name, species, weight from "
 								+ animalTableName + " where id_class=" + id);
 				while (rs1.next()) {
-					String aname = rs.getString("name");
-					String aspecies = rs.getString("species");
-					double aweight = rs.getDouble("weight");
+					String aname = rs1.getString("name");
+					String aspecies = rs1.getString("species");
+					double aweight = rs1.getDouble("weight");
 					animalgettall = new Animal(aname, aspecies, aweight);
 					zk.addAnimal(classgetall, animalgettall);
 				}
 
 			}
-			zk.printAnimals();
+			
 		} catch (SQLException e) {
 
 			e.printStackTrace();
@@ -206,10 +209,10 @@ public class HsqlDataManger implements DataManager {
 	}
 
 	public void save_animals_register_hsqldb() {
+
 		for (Class c : zk.animals.keySet())
 			for (Animal a : zk.animals.get(c)) {
 				addOrAnimalUpdate(c, a);
 			}
 	}
-
 }
--- src/pl/com/zoo/main/Zoo.java
+++ src/pl/com/zoo/main/Zoo.java
@@ -2,37 +2,17 @@ package pl.com.zoo.main;
 
 import pl.com.zoo.basic.Animal;
 import pl.com.zoo.basic.Class;
+import pl.com.zoo.data.db.HsqlDataManger;
 
 public class Zoo {
 
 	public static void main(String[] args) {
 		System.out.println("Witaj w ZOO!\n\n\n");
 
-		ZooKeeper keeper = new ZooKeeper();
-		// keeper.readAnimalsFromFile();
-		keeper.makeAnimalsRegister();
-		keeper.printAnimals();
-		keeper.saveAnimalRegister();
-		
-		
-//		
-//		Animal orf = new Animal("Orf", "Ogr", 0.3);
-//		Class tak = new Class("ogr");
-//		Animal goon1 = new Animal("Goonasdasdasd", "skorpion", 0.3);
-//		Class tak1 = new Class("arachnids");
-//		keeper.addAnimal(tak, orf);
-//		keeper.addAnimal(tak1, goon1);
-//		keeper.readAnimalsFromFile();
-//		keeper.printAnimals();
+		HsqlDataManger manager = new HsqlDataManger();
+		manager.getAll();
 
-//		System.out.println("Usuwamy orfa!\n\n\n");
-//		keeper.removeAnimal(orf);
-//		keeper.readAnimalsFromFile();
-//		keeper.printAnimals();
-		// System.out.println("\n\nZapis/odczyt z pliku...\n\n");
-		// keeper.saveAnimalsToFile();
-		// keeper.readAnimalsFromFile();
-		// keeper.printAnimals();ssssadasd
+		
 	}
 
 }
