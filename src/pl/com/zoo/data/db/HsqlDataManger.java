5d4911338aaa736c57dd83be5d4cd44c1839fe89
goon <goon@debian.(none)>
2010-11-12 15:06:57 +0100

    2010111201

--- src/pl/com/zoo/data/db/HsqlDataManger.java
+++ src/pl/com/zoo/data/db/HsqlDataManger.java
@@ -1,8 +1,7 @@
 package pl.com.zoo.data.db;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
 
+import static org.junit.Assert.fail;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,6 +9,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -17,16 +18,20 @@ import pl.com.zoo.basic.Animal;
 import pl.com.zoo.basic.Class;
 import pl.com.zoo.data.DataManager;
 
+
 public class HsqlDataManger implements DataManager {
 	private Connection con;
 	private String animalTableName = "animal";
 	private String classTableName = "class";
 	private Statement stmt;
-	
+	private String host = "localhost";
+	Animal animalgettall;
+	Class classgetall;
+
 	private void makeConnection() {
 		try {
-			con = DriverManager.getConnection(
-					"jdbc:hsqldb:hsql://localhost/zoodb", "SA", "");
+			con = DriverManager.getConnection("jdbc:hsqldb:hsql://" + host
+					+ "/zoodb", "SA", "");
 		} catch (SQLException e) {
 			e.printStackTrace();
 			fail("Wyjatek przy polaczeniu do bazy.");
@@ -53,9 +58,9 @@ public class HsqlDataManger implements DataManager {
 			ps.setString(2, animal.getSpecies());
 			ps.setDouble(3, animal.getWeight());
 			ps.setInt(4, addOrUpdateClass(cl));
-			//ps.executeUpdate();
+			// ps.executeUpdate();
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 		}
 		return false;
@@ -63,39 +68,99 @@ public class HsqlDataManger implements DataManager {
 
 	@Override
 	public void removeAnimal(Animal animal) {
-		// TODO Auto-generated method stub
+		try {
+			PreparedStatement ps = con.prepareStatement("Delete from "
+					+ animalTableName + " where name='?'");
+			ps.setString(1, animal.getName());
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public int addOrUpdateClass(Class cl) {
 		try {
-			PreparedStatement ps = con.prepareStatement("Insert into " +classTableName + "(name) values (?)");
+			PreparedStatement ps = con.prepareStatement("Insert into "
+					+ classTableName + "(name) values (?)");
 			ps.setString(1, cl.getName());
 			ps.executeUpdate();
-			ResultSet rs = stmt.executeQuery("SELECT MAX(ID) from " + classTableName);
-			int maxId = 0;
-			if(rs.next())
-				return rs.getInt(1);		
+			ResultSet rs = stmt.executeQuery("SELECT MAX(ID) from "
+					+ classTableName);
+			if (rs.next())
+				return rs.getInt(1);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return -1;
-		
 
 	}
 
 	@Override
 	public void removeClass(Class cl) {
-		// TODO Auto-generated method stub
+		try {
+			ResultSet rs = stmt.executeQuery("SELECT id from " + classTableName
+					+ " where name='" + cl.getName() + "'");
+
+			PreparedStatement ps1 = con.prepareStatement("Delete from "
+					+ animalTableName + " where id_class=?");
+			ps1.setInt(1, rs.getInt(1));
+			ps1.executeUpdate();
+
+			PreparedStatement ps = con.prepareStatement("Delete from "
+					+ classTableName + " where name='?'");
+			ps.setString(1, cl.getName());
+			ps.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public Map<Class, Set<Animal>> getAll() {
-		// TODO Auto-generated method stub
+		Map<Class, Set<Animal>> animals = new HashMap<Class, Set<Animal>>();
+		try {
+			ResultSet rs = stmt.executeQuery("SELECT id, name from "
+					+ classTableName);
+
+			while (rs.next()) {
+				String name = rs.getString("name");
+				int id = rs.getInt("id");
+
+				classgetall = new Class(name);
+				Set<Animal> hs = new HashSet<Animal>();
+
+				ResultSet rs1 = stmt
+						.executeQuery("SELECT name, species, weight from "
+								+ animalTableName + " where id_class=" + id);
+				while (rs1.next()) {
+					String aname = rs.getString("name");
+					String aspecies = rs.getString("species");
+					double aweight = rs.getDouble("weight");
+					animalgettall = new Animal(aname, aspecies, aweight);
+					hs.add(animalgettall);
+				}
+				animals.put(classgetall, hs);
+
+			}
+			for (Class c : animals.keySet()) {
+				System.out.println(c);
+				for (Animal a : animals.get(c)) {
+					System.out.println(a);
+				}
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
 		return null;
+
 	}
 
 }
